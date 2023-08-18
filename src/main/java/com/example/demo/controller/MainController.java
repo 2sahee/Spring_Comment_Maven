5a827b9faa288f449adf304432fbc18eefa6fe20
2sahee <119823052+2sahee@users.noreply.github.com>
2023-08-18 19:05:27 +0900

    first

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.dao.CommentJdbcDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class MainController {
+    @Autowired
+    private CommentJdbcDAO commentJdbcDAO;
+    @GetMapping("/")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name, Model model) {
+
+        List<Map<String, ?>> commentList = commentJdbcDAO.selectAllCommentList();
+         Map<String, ?> cmt = commentList.get(0);
+
+
+        model.addAttribute("no", cmt.get("no"));
+        model.addAttribute("author", cmt.get("author"));
+        model.addAttribute("comment", cmt.get("comment"));
+
+        return "main";
+    }
+}
